544866371cbce859f8ea7f77ececf0850cdf5415
jwwei <jwwei0309@gmail.com>
2014-07-03 13:47:03 +0800

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Weiwei Jiang on 2014/5/10.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
