6530a19b195ba390ee35c324e02e85bd41ba7195
jwwei <jwwei0309@gmail.com>
2014-07-10 17:28:42 +0800

    Change in 7/10.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,10 +1,18 @@
 package com.example.helloworld;
 
+import java.util.Calendar;
+
 /**
- * Created by Weiwei Jiang on 2014/5/10.
+ * Created by Weiwei Jiang on 2014/7/10.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH) + 1;
+        System.out.println(month);
+        int weekday = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        System.out.println(weekday);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(day);
     }
 }
