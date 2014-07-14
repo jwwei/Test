82c27273ee5f8561d7e94bc83f5db8d79109a135
jwwei <jwwei0309@gmail.com>
2014-07-14 14:30:54 +0800

    Change in 7/14.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,18 +1,43 @@
 package com.example.helloworld;
 
-import java.util.Calendar;
+//import java.util.Calendar;
+import java.util.Stack;
 
 /**
  * Created by Weiwei Jiang on 2014/7/10.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        int month = calendar.get(Calendar.MONTH) + 1;
-        System.out.println(month);
-        int weekday = calendar.get(Calendar.DAY_OF_WEEK) - 1;
-        System.out.println(weekday);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        System.out.println(day);
+        int[] height = new int[]{2, 1, 5, 6, 2, 3, 2, 3};
+        System.out.println(largestRectangleArea(height));
+    }
+
+
+    public static int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
+        int ans = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < height.length; ++i) {
+            //如果高度递增，那么一次入栈。
+            if (stack.isEmpty() || height[stack.peek()] <= height[i]) {
+                stack.push(i);
+            }
+            //如果当前柱比栈顶的低，那么把栈顶的拿出来，计算所有已经出栈的最大面积。
+            else {
+                int start = stack.pop();
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                ans = Math.max(ans, height[start] * width);
+                --i;
+            }
+        }
+
+        //循环过后栈中是递增的条目，计算在栈中递增条目的最大面积。
+        while (!stack.isEmpty()) {
+            int start = stack.pop();
+            int width = stack.isEmpty() ? height.length : height.length - stack.peek() - 1;
+            ans = Math.max(ans, height[start] * width);
+        }
+        return ans;
     }
 }
